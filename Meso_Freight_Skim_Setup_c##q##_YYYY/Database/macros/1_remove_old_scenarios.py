ef406b9dfab524b52309c6bdeab79bc17a5b3ae9
kcazzatoCMAP <kcazzato@cmap.illinois.gov>
2026-06-09 10:05:49 -0500

    Update 1_remove_old_scenarios.py
    change logic to use emme desktop and matrix api's

--- Meso_Freight_Skim_Setup_c##q##_YYYY/Database/macros/1_remove_old_scenarios.py
+++ Meso_Freight_Skim_Setup_c##q##_YYYY/Database/macros/1_remove_old_scenarios.py
@@ -8,11 +8,14 @@ import inro.modeller as _m
 import pandas as pd
     
 def main():
+    scenario = sys.argv[1]
+    output_dir = os.getcwd() + f"\\output_data\\{scenario}"
+    report_dir = os.getcwd() + f"\\reports\\{scenario}"
+
     # Define the path to the Emme project (.emp file)
     empFl = 'MesoFreightNetwork.emp'
     directory = os.getcwd().replace('\\Database','')
     empFile = os.path.join(directory,empFl)
-    print(empFile)
     # start a dedicated instance of Emme Desktop connected to the specified project
     desktop = _app.start_dedicated(
         visible=True,
@@ -23,43 +26,46 @@ def main():
     # Connect to the Modeller
     modeller = _m.Modeller(desktop=desktop)
     emmebank = modeller.emmebank
+    scenario = modeller.scenario
+    data_explorer = desktop.data_explorer()
+    database = data_explorer.active_database()
     change_scenario = modeller.tool('inro.emme.data.scenario.change_primary_scenario')
     create_scenario = modeller.tool('inro.emme.data.scenario.create_scenario')
     delete_scenario = modeller.tool('inro.emme.data.scenario.delete_scenario')
     delete_matrix = modeller.tool('inro.emme.data.matrix.delete_matrix')
-
-    # Obtain list of matrix values to be deleted
-    inMatNames =  "input_data/matrix.in"
-    in_MatNames = pd.read_csv(inMatNames, sep = '(\d+)', skiprows = 11, header=None, engine='python')
-    in_MatNames.columns=['mfnm', 'mf', 'abbrev', 'skip', 'desc']
-    matList = in_MatNames['mf'].values.tolist()
-
-    # Create empty scenario if id does not exist
-    sEmpty = create_scenario(scenario_id=1,
-                        scenario_title="empty scenario",
-                        overwrite = True)
     
-    # Set current scenario to empty scenario
-    change_scenario(scenario=1)
-
-    # Delete all matrices
-    for mat in matList:
-        stMat = 'mf' + str(mat)
+    try:
+        # Create empty scenario if id does not exist
+        sEmpty = create_scenario(scenario_id=1,
+                            scenario_title="empty scenario",
+                            overwrite = True)
+        
+        # Set current scenario to empty scenario
+        change_scenario(scenario=1)
+    except:
+        change_scenario(scenario=1)
+    
+    # Delete all matrices 
+    for matrix in emmebank.matrices():
+        stMat = str(matrix.id)
         try:
             delMat = emmebank.matrix(stMat)
             delete_matrix(matrix=delMat) 
         except:
-            None
+            print(f'matrix {stMat} NOT DELETED')
 
     # Delete Scenarios
-    scen = 2
-    while scen < 213:
-        try:
-            delScen = emmebank.scenario(scen)
-            delete_scenario(scenario=delScen)
-        except:
-            None
-        scen=scen+1
+    for scenario in database.scenarios():
+        if scenario.number() != 1:
+            try:
+                delScen = emmebank.scenario(scenario.number())
+                delete_scenario(scenario=delScen)
+            except:
+                print(f'DID NOT DELETE SCENARIO {scenario.number()}')
+
+    # Remove output folder and reports 
+    if os.path.isdir(output_dir): shutil.rmtree(output_dir)
+    if os.path.isdir(report_dir): shutil.rmtree(report_dir)
 
     
 if __name__ == '__main__':
